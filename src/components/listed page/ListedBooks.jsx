b48b262340b925c8d5b99f0c867545120e785212
NahidRuhan <dilal328@gmail.com>
2024-04-05 05:28:16 +0600

    finally implemented sorting

--- src/components/listed page/ListedBooks.jsx
+++ src/components/listed page/ListedBooks.jsx
@@ -11,17 +11,56 @@ const ListedBooks = () => {
     const books = useLoaderData();
     const [readBooks, setReadBooks] = useState([]);
     const [wishlistBooks, setWishlistBooks] = useState([]);
-    console.log(readBooks)
-    console.log(wishlistBooks)
+    const [displayReadBooks, setDisplayReadBooks] = useState([]);
+    const [displayWishlistBooks, setDisplayWishlistBooks] = useState([]);
 
-    useEffect(()=>{
-        const readId = getReadData();
-        const wishlistId = getWishlistData();  
-        const readBooks = books.filter(book => readId.includes(book.bookId));
-        const wishlistBooks = books.filter(book => wishlistId.includes(book.bookId));
-        setReadBooks(readBooks);
-        setWishlistBooks(wishlistBooks);      
-    },[books])
+    useEffect(() => {
+      const readId = getReadData();
+      const wishlistId = getWishlistData();
+      const readBooksFiltered = books.filter(book => readId.includes(book.bookId));
+      const wishlistBooksFiltered = books.filter(book => wishlistId.includes(book.bookId));
+      
+      setReadBooks(readBooksFiltered);
+      setWishlistBooks(wishlistBooksFiltered);
+      
+      // Sort books by rating by default
+      sortBooksByRating(readBooksFiltered, wishlistBooksFiltered);
+  }, [books]);
+
+  const sortBooksByRating = (readBooks, wishlistBooks) => {
+    const readBooksSorted = [...readBooks].sort((a, b) => b.rating - a.rating);
+    const wishlistBooksSorted = [...wishlistBooks].sort((a, b) => b.rating - a.rating);
+    setDisplayReadBooks(readBooksSorted);
+    setDisplayWishlistBooks(wishlistBooksSorted);
+};
+
+const sortBooksByYear = (readBooks, wishlistBooks) => {
+    const readBooksSorted = [...readBooks].sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+    const wishlistBooksSorted = [...wishlistBooks].sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+    setDisplayReadBooks(readBooksSorted);
+    setDisplayWishlistBooks(wishlistBooksSorted);
+};
+
+const sortBooksByPage = (readBooks, wishlistBooks) => {
+    const readBooksSorted = [...readBooks].sort((a, b) => b.totalPages - a.totalPages);
+    const wishlistBooksSorted = [...wishlistBooks].sort((a, b) => b.totalPages - a.totalPages);
+    setDisplayReadBooks(readBooksSorted);
+    setDisplayWishlistBooks(wishlistBooksSorted);
+};
+
+
+  const handleRating = () => {
+      sortBooksByRating(readBooks, wishlistBooks);
+  };
+    const handleYear = ()=> {
+
+      sortBooksByYear(readBooks, wishlistBooks);
+
+    }
+    const handlePage = ()=> {
+      sortBooksByPage(readBooks, wishlistBooks);
+
+    }
 
     return (
         <div className="space-y-14">
@@ -32,9 +71,9 @@ const ListedBooks = () => {
             <div className="dropdown dropdown-bottom">
   <div tabIndex={0} role="button" className="btn m-1 bg-[#23BE0A]">Filter <span><IoIosArrowDown /></span></div>
   <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 text-left space-y-2 font-bold">
-    <li>Rating</li>
-    <li>Year</li>
-    <li>Number of Pages</li>
+    <li className='hover:cursor-pointer' onClick={handleRating}>Rating</li>
+    <li className='hover:cursor-pointer' onClick={handleYear}>Year</li>
+    <li className='hover:cursor-pointer' onClick={handlePage}>Number of Pages</li>
   </ul>
 </div>
             </div>
@@ -49,12 +88,12 @@ const ListedBooks = () => {
   
       <TabPanel>
   {
-    readBooks.map(bookuntola => <ListItem key={bookuntola.bookId} bookuntola={bookuntola}></ListItem>)
+    displayReadBooks.map(bookuntola => <ListItem key={bookuntola.bookId} bookuntola={bookuntola}></ListItem>)
   }
 </TabPanel>
 <TabPanel>
   {
-    wishlistBooks.map(bookuntola => <ListItem key={bookuntola.bookId} bookuntola={bookuntola}></ListItem>)
+    displayWishlistBooks.map(bookuntola => <ListItem key={bookuntola.bookId} bookuntola={bookuntola}></ListItem>)
   }
 </TabPanel>
     </Tabs>
